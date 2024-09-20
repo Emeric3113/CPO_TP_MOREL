13bc61898a7a4b27a50c1fe0e025fa38923fc5f4
emeri <emeri@LAPTOP-61DJFKQQ>
2024-09-20 17:36:44 +0200

    TP1-Exo1 Saisie et manipulation de nombres

---
+++ TP1-Exo1-manipNombresInt/src/tp1/exo1/manipnombresint/TP1Exo1ManipNombresInt.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1.exo1.manipnombresint;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emeri
+ */
+public class TP1Exo1ManipNombresInt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb1;
+        int nb2;
+        
+        Scanner sc = new Scanner(System.in);
+        nb1 = sc.nextInt();
+        nb2 = sc.nextInt();
+        // TODO code application logic here
+    }
+    
+}

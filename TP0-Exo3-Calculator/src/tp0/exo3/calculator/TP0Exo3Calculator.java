3af6d19a3784a3ceb5edbf40ed0198d1b718feda
emeri <emeri@LAPTOP-61DJFKQQ>
2024-09-20 17:52:54 +0200

    TP1-Exo1 Saisie et manipulation de nombres

--- TP0-Exo3-Calculator/src/tp0/exo3/calculator/TP0Exo3Calculator.java
+++ TP0-Exo3-Calculator/src/tp0/exo3/calculator/TP0Exo3Calculator.java
@@ -4,6 +4,8 @@
  */
 package tp0.exo3.calculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emeri
@@ -14,6 +16,36 @@ public class TP0Exo3Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int operateur;
+        int operande1;
+        int operande2;
+        System.out.println("Choisissez un opérateur : \n1) add \n2) substract \n3) multiply \n4) divide \n5) modulo ");
+        Scanner sc = new Scanner(System.in);
+        operateur = sc.nextInt();
+        System.out.println("Choisissez un nombre :");
+        operande1 = sc.nextInt();
+        System.out.println("Choisissez un deuxième nombre :");
+        operande2 = sc.nextInt();
+    
+        switch(operateur){
+            case 1 :
+                System.out.println(operande1 + operande2);
+                break;
+            case 2 :
+                System.out.println(operande1 - operande2);
+                break;
+            case 3 :
+                System.out.println(operande1 * operande2);
+                break;
+            case 4 :
+                System.out.println((float)operande1 / (float)operande2);
+                break;
+            case 5 :
+                System.out.println(operande1 % operande2);
+                break;
+            default:
+                System.err.println("erreur de saisie");
+        }
         // TODO code application logic here
     }
     

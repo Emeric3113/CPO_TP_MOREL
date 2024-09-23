14d922a22e74b2ce7972aa4c8fe04d1539f274f5
emeri <emeri@LAPTOP-61DJFKQQ>
2024-09-23 11:39:04 +0200

    TP1-Exo2 Un convertisseur de températures

--- TP1-Exo2-convertisseur/src/tp1/exo2/convertisseur/TP1Exo2Convertisseur.java
+++ TP1-Exo2-convertisseur/src/tp1/exo2/convertisseur/TP1Exo2Convertisseur.java
@@ -18,29 +18,36 @@ public class TP1Exo2Convertisseur {
     public static void main(String[] args) {
         double temp;
         int conv;
+        boolean a;
+        a = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Saisissez une température :");
         temp = sc.nextDouble();
         System.out.println("Saissisez une coversion :");
         System.out.println("1. Kelvin vers Celcius\n" +"2. Farenheit vers Celcius\n" +"3. Celcius vers Farenheit\n" +"4. Kelvin vers Farenheit\n" +"5. Farenheit vers Kelvin");
         
-        while (true){
+        while (a){
             conv=sc.nextInt();
             switch(conv){
                 case 1:
                     System.out.println(KelvinVersCelcius(temp));
+                    a=false;
                     break;
                 case 2:
                     System.out.println(FarenheitVersCelcius(temp));
+                    a=false;
                     break;
                 case 3:
                     System.out.println(CelciusVersFarenheit(temp));
+                    a=false;
                     break;
                 case 4:
                     System.out.println(KelvinVersFarenheit(temp));
+                    a=false;
                     break;
                 case 5:
                     System.out.println(FarenheitVersKelvin(temp));
+                    a=false;
                     break;
                 default:
                     System.err.println("erreur de saisie");

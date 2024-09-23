b60fe5bab793becc068103d188af37212637790d
emeri <emeri@LAPTOP-61DJFKQQ>
2024-09-23 13:00:04 +0200

    TP1-Exo4 Statistiques

--- TP1-Exo4-Statistiques/src/tp1/exo4/statistiques/TP1Exo4Statistiques.java
+++ TP1-Exo4-Statistiques/src/tp1/exo4/statistiques/TP1Exo4Statistiques.java
@@ -18,18 +18,27 @@ public class TP1Exo4Statistiques {
      */
     public static void main(String[] args) {
         int m;
-        int alea;
-        int i;
+        int a; int b; int c; int d; int e; int f;
+        a=0; b=0; c=0; d=0; e=0; f=0;
         Scanner sc = new Scanner(System.in);
         Random generateurAleat = new Random();
         int[] tab = new int[6];
-        System.out.println("Saisissez un entier :");
+        System.out.println("Saisissez un entier entre 0 et 5 :");
         m = sc.nextInt();
         
-        for (i = 0; i<=m; i++){
+        for (int i = 0; i<=m; i++){
             tab[i] = generateurAleat.nextInt(5);
-            System.out.println(tab[i]);
         }
+        for (int i = 0; i< m; i++){
+            if (tab[i]==0){a=a+1;}
+            if (tab[i]==1){b=b+1;}
+            if (tab[i]==2){c=c+1;}
+            if (tab[i]==3){d=d+1;}
+            if (tab[i]==4){e=e+1;}
+            if (tab[i]==5){f=f+1;}
+        }
+        System.out.println("0 : "+ (a*100)/6+"%\n"+"1 : "+ (b*100)/6+"%\n"+"2 : "+ (c*100)/6+"%\n"+"3 : "+ (d*100)/6+"%\n"+"4 : "+ (e*100)/6+"%\n"+"5 : "+ (f*100)/6+"%");
+        
         
             
         

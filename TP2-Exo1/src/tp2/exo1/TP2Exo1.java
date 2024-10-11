5c4690fc38c655f7a5b6e436bffa7e983423c367
emeri <emeri@LAPTOP-61DJFKQQ>
2024-10-11 09:15:16 +0200

    TP2-Exo1 Création d’une classe et de quelques objets associés

--- TP2-Exo1/src/tp2/exo1/TP2Exo1.java
+++ TP2-Exo1/src/tp2/exo1/TP2Exo1.java
@@ -14,19 +14,29 @@ public class TP2Exo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0;
-        uneBiere.brasserie = "Dubuisson";
-        uneBiere.ouverte = false;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson");
         uneBiere.lireEtiquette();
+        System.out.println("Ouverture : " + uneBiere.Decapsuler());
         
-        BouteilleBiere deuxiemeBiere = new BouteilleBiere();
-        uneBiere.nom = "Leffe";
-        uneBiere.degreAlcool = 6.6;
-        uneBiere.brasserie = "Abbaye de Leffe";
-        uneBiere.ouverte = false;
-        uneBiere.lireEtiquette();        
+        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe");
+        deuxiemeBiere.lireEtiquette();
+        System.out.println("Ouverture : " + deuxiemeBiere.Decapsuler());
+        
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("3",99.0,"brasserie 1");
+        troisiemeBiere.lireEtiquette();
+        System.out.println("Ouverture : " + troisiemeBiere.Decapsuler());
+        
+        BouteilleBiere quatriemeiemeBiere = new BouteilleBiere("4",47.1,"brasserie 2");
+        quatriemeiemeBiere.lireEtiquette();
+        System.out.println("Ouverture : " + quatriemeiemeBiere.Decapsuler());
+        
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("5",21.4,"brasserie 3");
+        cinquiemeBiere.lireEtiquette();
+        System.out.println("Ouverture : " + cinquiemeBiere.Decapsuler());
+        
+        
+        
+        System.out.println(uneBiere);
     }
     
 }

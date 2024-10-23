a8c8967811fc336112c957238cff30cf6c7ec665
emeri <emeri@LAPTOP-61DJFKQQ>
2024-10-23 10:23:57 +0200

    TP2-Exo3 Manipulation des objets (par leur référence)

---
+++ TP2-Exo3/src/tp2/exo3/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2.exo3;
+
+/**
+ *
+ * @author emeri
+ */
+public class Moussaka {
+    int nbCalories;
+
+    public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+    
+}

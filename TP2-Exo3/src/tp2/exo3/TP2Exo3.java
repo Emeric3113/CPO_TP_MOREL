f66f7cfec8fda415dbc20a4e71d81e0ae63a529c
emeri <emeri@LAPTOP-61DJFKQQ>
2024-10-23 10:46:25 +0200

    TP2-Exo3 Manipulation des objets (par leur référence)

--- TP2-Exo3/src/tp2/exo3/TP2Exo3.java
+++ TP2-Exo3/src/tp2/exo3/TP2Exo3.java
@@ -17,10 +17,23 @@ public class TP2Exo3 {
         // TODO code application logic here
         Tartiflette assiette1 = new Tartiflette(500) ;
         Tartiflette assiette2 = new Tartiflette(600) ;
-        Tartiflette assiette3 = assiette2 ;
+        Tartiflette assiette3 = assiette2;
+        Tartiflette a = assiette1;
+        assiette1 = assiette2;
+        assiette2 = a;
         
         System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
         System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
+        
+        Moussaka[] tab = new Moussaka[10];
+        for (int i = 0; i < tab.length; i++){ 
+            tab[i] = new Moussaka((i+1)*100);
+            Moussaka b = tab[i];
+            System.out.println("Moussaka "+ (i+1) +" : "+ b.nbCalories + " calories");
+        }
+
+        
     }
     
 }
